d093b57a3f5d2598cd7c3b1e922da800c3831a56
seemsso <W8something@yandex.com>
2021-08-02 18:51:08 +0500

    test changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./Nullstyles/null.css";
 import Main from "./components/Main/Main";
-// import Header from "./components/Header/Header";
-// import Navigation from "./components/Navigation/Navigation";
-// import Profile from "./components/Profile/Profile";
-// import Footer from "./components/Footer/Footer";
+// import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
