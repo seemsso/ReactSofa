490637011af42af8bcab991705e2a4f9bdedeccd
seemsso <W8something@yandex.com>
2021-08-13 19:01:31 +0500

    changes with Sofas page in catalog

--- src/components/Catalog/Sofas/ImgSofas/ImgSofas.jsx
+++ src/components/Catalog/Sofas/ImgSofas/ImgSofas.jsx
@@ -1,10 +1,28 @@
+import sofach from "./ImgSofas.module.scss";
+
 const ImgSofas = (props) => {
   return (
-    <div>
+    <div className={sofach.container}>
       <base href="/"></base>
       <div>
         <img height={435} width={555} src={props.src} alt={props.alt}></img>
-        <span>{props.alt}</span>
+        <div className={sofach.footer}>
+          <div>
+            <div className={sofach.name}>{props.alt}</div>
+            <div className={sofach.fav}>
+              <img src="/images/icons/fav.svg" alt="fav" />
+            </div>
+          </div>
+          <div className={sofach.right}>
+            <div>
+              <span className={sofach.price}>1199</span>
+              <span>₽</span>
+            </div>
+            <div className={sofach.cartadd}>
+              <img src="images/icons/cartadd.svg" alt="add" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
