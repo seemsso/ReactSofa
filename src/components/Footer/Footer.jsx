df76f02c891b92afe16b588f41e1af568eaa3f7a
seemsso <W8something@yandex.com>
2021-08-16 19:56:17 +0500

    correcting in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import f from "./Footer.module.scss";
 
 const Footer = () => {
@@ -5,13 +6,23 @@ const Footer = () => {
     <div className={f.container}>
       <header className={f.header}>
         <div className={f.container}>
-          <div className={f.title}>MY SOFA | диваны и кресла</div>
+          <NavLink to="/main" className={f.active_main}>
+            MY SOFA | диваны и кресла
+          </NavLink>
           <div className={f.nav}>
             <nav>
               <ul className={f.ul}>
-                <li>Каталог</li>
+                <li className={f.catalog}>
+                  <NavLink to="/catalog" activeClassName={f.active_catalog}>
+                    Каталог
+                  </NavLink>
+                </li>
                 <li>О проекте</li>
-                <li>Доставка</li>
+                <li>
+                  <NavLink to="/shopcart" activeClassName={f.active_shopcart}>
+                    Доставка
+                  </NavLink>
+                </li>
                 <li>Контакты</li>
               </ul>
             </nav>
